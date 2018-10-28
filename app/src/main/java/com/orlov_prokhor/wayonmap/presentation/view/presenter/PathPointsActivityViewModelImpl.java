868e262e3cef00d52ae5e68c37e73ee16f0d8d32
Prokhor <orlov_proxor@mail.ru>
2018-10-28 11:55:36 +0600

    final commit

--- app/src/main/java/com/orlov_prokhor/wayonmap/presentation/view/presenter/PathPointsActivityViewModelImpl.java
+++ app/src/main/java/com/orlov_prokhor/wayonmap/presentation/view/presenter/PathPointsActivityViewModelImpl.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
-import android.util.Log;
-import android.widget.Toast;
 import com.orlov_prokhor.wayonmap.domain.PathPoint;
 import com.orlov_prokhor.wayonmap.domain.repository.PathPointsRepository;
 import com.orlov_prokhor.wayonmap.presentation.AndroidApplication;
@@ -24,7 +22,7 @@ public class PathPointsActivityViewModelImpl extends AndroidViewModel implements
     super(application);
     ((AndroidApplication) application).getApplicationComponent().inject(this);
 
-    Log.i("delete log  ", pointsRepository.toString());
+
   }
 
   @Override
@@ -34,18 +32,14 @@ public class PathPointsActivityViewModelImpl extends AndroidViewModel implements
 
   @Override
   public void showPath() {
-    Toast.makeText(this.getApplication(), "showPath", Toast.LENGTH_LONG).show();
+
     pointsRepository.pathPoints().subscribe(
         list -> {
-          Log.i("list: ", "" + list.size());
+
           pathPointList.set(list);
         }
         , err -> {
-          /*try {
-            Log.i("list err: ", err.getMessage());
-          } catch (Exception e) {
-            Log.i("list err222: ", e.getMessage());
-          }*/
+
         });
   }
 }
